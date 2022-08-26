0c7f10846879f96858975dcbf5f5583efbaf2a4d
Tanya Matseonzhek <matseonzhek@gmail.com>
2022-08-26 23:50:53 +0400

    stage1 module6 task1 QUALITY IMPROVED

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -2,23 +2,19 @@ package com.epam.mjc.io;
 
 import java.io.*;
 
-
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        String [] profileData = new String[4];
+        String[] profileData = new String[4];
         String inString;
-        try(BufferedReader inStream = new BufferedReader(new InputStreamReader(new FileInputStream(file)))){
+        try (BufferedReader inStream = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
 
-            for (int i = 0; i < 4 ; i++) {
+            for (int i = 0; i < 4; i++) {
                 inString = inStream.readLine();
-                profileData[i] = inString.substring(inString.indexOf(':')+2).trim();
+                profileData[i] = inString.substring(inString.indexOf(':') + 2).trim();
             }
-        } catch (IOException ioException){
+        } catch (IOException ioException) {
             System.out.println("Could not access data " + ioException);
-        }
-        for (int i = 0; i < 4; i++) {
-            System.out.println(profileData[i]);
         }
         return new Profile(profileData[0], Integer.parseInt(profileData[1]), profileData[2], Long.parseLong(profileData[3]));
     }
